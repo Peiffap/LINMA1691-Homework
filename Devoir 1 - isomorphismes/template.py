d602329450a23a712467a30af1a85841cf9f61a7
Nizar <niz.bousselmi@gmail.com>
2018-10-07 19:05:59 +0200

    Add comments

--- Devoir 1 - isomorphismes/template.py
+++ Devoir 1 - isomorphismes/template.py
@@ -93,91 +93,82 @@ def color_k_neigh(A, k):
 
     n = len(A) #Longueur de A
     
-    #Matrice nulle
+    # Zero matrix (take the role of a "empty matrix")
     AkNext = []
     for x in range(0, n):
         AkNext.append([])
         for y in range(0, n):
             AkNext[x].append(0)
-    #List a renvoyer
+            
+    # List to return
     tuplist = []
     for x in range(0, n):
         tuplist.append([])
     
-    #Copier ELEMENT PAR ELEMENT A dans Ak
+    #Copy ELEMENT by ELEMENT of A in Ak
     Ak = []
     for x in range(0, n):
         Ak.append([])
         for y in range(0, n):
             Ak[x].append(A[x][y])
-    
-    #Garde en memoire les chemins deja comptes: 1 = pas encore compte, 0 = counted       
+     
+    # Keep a track of paths already counted: klist[i][j] = 1 if not counted and 0 if counted    
     klist = []
     for x in range(0, n):
         klist.append([])
         for y in range(0, n):
             klist[x].append(1)
    
-
-    #d itere toutes les valeurs de k, de 0 a k compris
+    # d iterate all values of k, from 0 to k included
     for d in range(0,k+1):
         
-        #Cas singulier k == 0 : tout les noeuds sont a une distance 0 de eux meme
+        # Particular case k == 0
         if d == 0:
             for i in range(0, n):
                 tuplist[i].append((0, sum(A[i])))
                 klist[i][i] = 0
-
-            
-        #Cas singulier k == 1 : on utilise la matrice A^1
+                
+        # Particular case k ==1. We use matrix A^1
         elif d == 1:
             for i in range(0, n):
                 for j in range(0, n):
+                    # Here, shortest path ij has a length of 1
                     if klist[i][j] == 1 and A[i][j] != 0:
                         tuplist[i].append((1, sum(A[j])))
                         tuplist[i] = sorted(tuplist[i])
                         klist[i][j] = 0
+                    # Here, shortest path ij (actually it is ii) has a length less than 1 but we add it anyway
                     elif klist[i][j] == 0:
                         tuplist[i].append((1, sum(A[j])))
                         tuplist[i] = sorted(tuplist[i])
         
-        #Cas quelconque
+        # Usual case
         else:
-
-            #Calcul de A^k
+            # Step 1: Computation of A^k
             for i in range(0, n):
                 for j in range(0, n):
                        for m in range(0, n):
-                           #Matrice nulle = Ak * A
+                           # Empty matrix = Ak * A
                            AkNext[i][j] += Ak[i][m] * A[m][j]
-
+            # Copy ELEMENT by ELEMENT AkNext (=Ak+1) in Ak
             for x in range(0, n):
                 for y in range(0, n):
-                    #Copier ELEMENT PAR ELEMENT AkNext (=Ak+1) dans Ak et mettre AkNext à 0
                     Ak[x][y] = AkNext[x][y]
-                    AkNext[x][y] = 0
             
-            #Construction de tuplist
+            # Step 2 : Construction of tuplist
             for i in range(0, n):
                 for j in range(0, n):
-                   # if i == 0:
-                        
-                       # print("klist={}".format(klist))
-                       # print("Ak={}".format(Ak))
+                    # Here, shortest path ij has a length less than d but we add it anyway
                     if klist[i][j] == 0:
-                       # print("if1, j = {}".format(j))
                         tuplist[i].append((d, sum(A[j])))
                         tuplist[i] = sorted(tuplist[i])
-                        
+                    # Here, shortest path ij has a length of d    
                     elif klist[i][j] == 1 and Ak[i][j] != 0:
-                       # print("if2, j = {}".format(j))
                         tuplist[i].append((d, sum(A[j])))
                         tuplist[i] = sorted(tuplist[i])
                         klist[i][j] = 0
-                    """elif klist[i][j] == 0:
-                        tuplist[i].append((d, sum(A[j])))
-                        tuplist[i] = sorted(tuplist[i])"""
                       
+    # Finally we sort each line of tuplist
     for i in range (0, n):
         tuplist[i] = tuple(tuplist[i])
 
