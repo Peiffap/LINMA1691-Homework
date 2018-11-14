7b69d24f052576a8ff5f40906cc5f04410abf78f
Gilles Peiffer <gilles.peiffer@outlook.com>
2018-11-14 23:09:25 +0100

    [homework-4] finish devoir 4, ford-fulkerson

--- Devoir 4/template.py
+++ Devoir 4/template.py
@@ -21,20 +21,37 @@ def matching(T, friends, hiding_places):
     """
     
     ans = 0
-
     adj_mat = []
+
     for i in range(len(friends)):
         adj_mat.append([])
         for j in range(len(hiding_places)):
             distx = hiding_places[j][0] - friends[i][0]
             disty = hiding_places[j][1] - friends[i][1]
             time = math.sqrt(distx*distx + disty*disty)/friends[i][2]
-            adj_mat[i].append(time)
+            if time <= T:
+                adj_mat[i].append(1)
+            else:
+                adj_mat[i].append(0)
 
-    matching = []
-    while len(matching) < min(len(friends), len(hiding_places)):
+    fr = len(friends)
+    hp = len(hiding_places)
 
-        a = 1
+    def matching_exists(u, matchr, seen):
+        for v in range(hp):
+            if adj_mat[u][v] == 1 and not seen[v]:
+                seen[v] = True
+                if matchr[v] == -1 or matching_exists(matchr[v], matchr, seen):
+                    matchr[v] = u
+                    return True
+        return False
+
+    matchr = [-1] * hp
+
+    for i in range(fr):
+        seen = [False] * hp
+        if matching_exists(i, matchr, seen):
+            ans += 1
 
     return ans
 
